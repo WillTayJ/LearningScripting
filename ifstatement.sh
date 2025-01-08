41c71cb436415b4350e1f4dc6128601df455349e
Will Taylor <williamtaylorj@gmail.com>
2025-01-08 14:13:28 -0500

    modified if statment basics file

--- ifstatement.sh
+++ ifstatement.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+#set age
 age=21
 
+#check if age is greater than 20
 if [ $age -gt 20 ]
 then
 	echo "You are $age years old, so you are old enough to drink."
 else
 	echo "You are $age years old, so you are not old enough to drink"
+fi
+
+#check if file exists
+if [ -f /myfile ]
+then
+	echo "this file exists."
+else
+	echo "this file does not exist."
 fi
