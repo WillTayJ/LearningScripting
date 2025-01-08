a9d72b768477d28ca19fc8e0d51801971ccca47c
Will Taylor <williamtaylorj@gmail.com>
2025-01-08 14:01:13 -0500

    bash script on basics of if statements

---
+++ ifstatement.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+age=21
+
+if [ $age -gt 20 ]
+then
+	echo "You are $age years old, so you are old enough to drink."
+else
+	echo "You are $age years old, so you are not old enough to drink"
+fi
