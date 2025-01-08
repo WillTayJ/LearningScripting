f8b30df13ddf67e0e2171a3ecd00a31a927e78d5
Will Taylor <williamtaylorj@gmail.com>
2025-01-08 13:45:58 -0500

    adding bash script that shows the basics of variables and another bash script that shows how math experssions work in bash

---
+++ math.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#adding
+expr 30 + 10
+
+#subtraction
+expr 30 - 10
+
+#division
+expr 30 / 10
+
+#multiplication
+expr 30 \* 10
---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+myname="Will"
+myage="84"
+
+echo "Hello, my name is $myname."
+echo "I am $myage years old."
