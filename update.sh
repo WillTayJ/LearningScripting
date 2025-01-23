d80a0f4ac669eb0b6e3071775a872c7f713c0801
Will Taylor <williamtaylorj@gmail.com>
2025-01-23 15:55:48 -0500

    updated script to send update logs and error logs to specific files and a function to check the exit code if not equal to 0

--- update.sh
+++ update.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
+
+#if there is an error in the update this function will run to check the exit code
+check_exit_code(){
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, Please check the $errorlog file."
+	fi
+}
 
 #checks if redhat system
 if grep -q "REDHAT" $release_file
 then
 	#update for redhat systems
-	sudo dnf update -y
+	sudo dnf update -y >>$logfile 2>>$errorlog
+	check_exit_code
 
 #check if ubuntu or debian system
 elif grep -q "ubuntu" $release_file || grep -q "debian" $release_file
 then
 	#update for ubuntu or debian systems
-	sudo apt update -y
-	sudo apt update -y
-	sudo apt dist-upgrade -y
+	sudo apt update -y >>$logfile 2>>$errorlog
+	check_exit_code
+	sudo apt dist-upgrade -y >>$logfile 2>>$errorlog
+	check_exit_code
 
 else
 	#if script is not compatible for redhat, debian, or ubuntu systems the comment will show
